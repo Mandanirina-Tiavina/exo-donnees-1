0e73db558b55a88df22fcbe993e8e3e9051245e0
Manda Tiavina <solontianamandanirina@gmail.com>
2025-11-17 19:22:56 +0400

    feat: answer of the question 4

--- donnees.sql
+++ donnees.sql
@@ -4,5 +4,8 @@ SELECT id, first_name, last_name FROM Employee WHERE team_id IS NULL;
 -- exo 2
 SELECT Employee.id, Employee.first_name, Employee.last_name FROM Leave JOIN Employee ON Employee.id = Leave.employee_id where Leave.id is NULL;
 
---exo3
+-- exo 3
 SELECT Leave.id, Leave.start_date, Leave.end_date, Employee.first_name, Employee.last_name, Team.name FROM Leave JOIN Employee ON Employee.id = Leave.employee_id JOIN Team ON Team.id = Employee.team_id;
+
+-- exo 4
+SELECT contract_type, COUNT(*) FROM Employee GROUP BY contract_type;
