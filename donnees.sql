6dc79e343f1ca5c857d5de85b36bf81c5baf69af
Manda Tiavina <solontianamandanirina@gmail.com>
2025-11-17 19:01:56 +0400

    feat: answer of the question 2

--- donnees.sql
+++ donnees.sql
@@ -1,2 +1,5 @@
 -- exo 1
 SELECT id, first_name, last_name FROM Employee WHERE team_id IS NULL;
+
+-- exo 2
+SELECT Employee.id, Employee.first_name, Employee.last_name FROM Leave JOIN Employee ON Employee.id = Leave.employee_id where Leave.id is NULL;
